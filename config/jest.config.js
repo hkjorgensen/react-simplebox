a3a7847d4d5473581bb4d4bc6bda5441f9e94683
Henrik Kok Jørgensen <henrikj@me.com>
2018-03-01 17:42:02 -0800

    Prettier

--- config/jest.config.js
+++ config/jest.config.js
@@ -3,5 +3,5 @@ const path = require('path')
 module.exports = {
   rootDir: path.join(__dirname, '..', 'src'),
   testPathIgnorePatterns: ['/node_modules/', '.eslintrc.js'],
-  setupTestFrameworkScriptFile: path.join(__dirname, 'setupJest.js')
+  setupTestFrameworkScriptFile: path.join(__dirname, 'setupJest.js'),
 }
