63a61c60c369333b24845e4b0b83cb5ed9744388
Henrik Kok Jørgensen <henrikj@me.com>
2018-03-01 21:35:00 -0800

    Add simple snapshot test

--- examples/row.js
+++ examples/row.js
@@ -1,1 +1,19 @@
-alert(1)
+import React from 'react'
+import { render } from 'react-dom'
+import { Grid, Row } from '../src/index'
+
+const App = () => (
+  <div style={{ minHeight: '100vh', display: 'flex' }}>
+    <Grid>
+      <Row size={48} boxStyle={{ background: 'tomato' }}>Header</Row>
+      <Row boxStyle={{ background: 'steelblue' }}>
+        Content
+      </Row>
+      <Row size={48} boxStyle={{ background: 'tomato' }}>
+        Footer
+      </Row>
+    </Grid>
+  </div>
+)
+
+render(<App />, document.getElementById('root'))
--- src/__tests__/row.js
+++ src/__tests__/row.js
@@ -1,3 +1,24 @@
-test('demo', () => {
-  expect(1).toBe(1)
+import puppeteer from 'puppeteer'
+let page, browser
+
+const DEBUG = false
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: !DEBUG })
+  page = await browser.newPage()
+  page.setViewport({
+    width: 800,
+    height: 600
+  })
+})
+
+afterEach(async () => {
+  browser.close()
+})
+
+test('Row', async () => {
+  await page.goto('http://localhost:8000/examples/row')
+  const screenshot = await page.screenshot()
+
+  expect(screenshot).toMatchImageSnapshot()
 })
